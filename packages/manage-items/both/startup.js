f68f03d7b684e06af4d91d116950260616093623
laurent <laurent.huron@gmail.com>
2018-01-01 01:57:19 +0800

    manage items main menu card

--- packages/manage-items/both/startup.js
+++ packages/manage-items/both/startup.js
@@ -1,5 +1,10 @@
 Meteor.startup( function () {
 	if ( Meteor.isClient ) {
-		utils.navigation({pluginRoute:`/manageItems`, pluginDisplayName:"Inventory"});
+		utils.navigation({
+			pluginRoute:`/manageItems`,
+			pluginDisplayName:"Inventory",
+			pluginDisplayImage: "/packages/laurenth_manage-items/resources/food-3048440_640.jpg",
+			pluginHomeCardInfo: "List and edit what's in your pantry. Easily find out if you have enough of something for that one recipe you're thinking about!"
+		});
 	}
 });
