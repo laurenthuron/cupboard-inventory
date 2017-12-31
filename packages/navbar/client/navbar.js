0adf1e4b49b5a47bd59545606660576686524d8d
laurent <laurent.huron@gmail.com>
2018-01-01 01:01:59 +0800

    Navbar readsfrom global utils for navigation links to display

--- packages/navbar/client/navbar.js
+++ packages/navbar/client/navbar.js
@@ -1,1 +1,11 @@
+Template.navigationBar.helpers({
+	navElements: function () {
+		return utils.navbar;
+	}
+});
 
+Template.navigationBar.events({
+	"click .navbar-collapse a" ( event, template ) {
+		$(".navbar-collapse").collapse('hide');
+	}
+});
