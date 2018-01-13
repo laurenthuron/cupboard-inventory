79f18f7a78b48ee3906786489fbd2131afd06202
laurent <laurent.huron@gmail.com>
2018-01-13 16:19:02 +0800

    Additonal utils functions. validURL and capitalize

--- packages/utils/utils.js
+++ packages/utils/utils.js
@@ -1,9 +1,27 @@
 utils = {};
+// Check if the string is a valid URL
+utils.validURL = function ( str ) {
+	const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+			'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+	return pattern.test( str );
+};
 
+// Capitalize the first letter of a string
+utils.capitalizeFirstLetter = function ( string ) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+// utils is accessible from both the server and the client but
+// will have different functions and properties depending on which side it is on.
+// This prevents exposing potentially private information to the client.
 if ( Meteor.isClient ) {
 	utils.navbar = [];
 	utils.pluginButtons = [];
-	utils.pluginDb = []
+	utils.pluginDb = [];
 	utils.navigation = function (plugin) {
 		check (plugin, Match.OneOf({
 			pluginRoute: String,
